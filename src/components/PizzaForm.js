2ad55764ed4fc19ad75baede1a4a8b4e301da57d
piaoyehong1107 <piaoyehong1107@gmail.com>
2020-09-22 00:03:48 -0500

    Done.

--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,37 +1,42 @@
 import React from "react"
 
-const PizzaForm = () => {
+const PizzaForm = (props) => {
+
   return(
       <div className="form-row">
         <div className="col-5">
             <input type="text" className="form-control" placeholder="Pizza Topping" value={
-                //Pizza Topping Should Go Here
-                null
-              }/>
+                props.selectedPizza.topping
+              } 
+              onChange={e=>{props.editForm(e.target.value, 'topping')}}/>
         </div>
         <div className="col">
-          <select value={null} className="form-control">
+          <select value={props.selectedPizza.size} className="form-control" onChange={e=>{props.editForm(e.target.value, 'size')}}>
             <option value="Small">Small</option>
-            <option value="Medium">Medium</option>
+            <option value="Medium">Medium</option>]
             <option value="Large">Large</option>
           </select>
         </div>
         <div className="col">
-          <div className="form-check">
-            <input className="form-check-input" type="radio" value="Vegetarian" checked={null}/>
+          <div className="form-check" onChange={e => {
+            props.editForm(e.target.value==='Vegetarian'? true: false, 'vegetarian')
+          }}>
+            <input className="form-check-input" type="radio" value="Vegetarian" checked={props.selectedPizza.vegetarian}/>
             <label className="form-check-label">
               Vegetarian
             </label>
           </div>
-          <div className="form-check">
-            <input className="form-check-input" type="radio" value="Not Vegetarian" checked={null}/>
+          <div className="form-check" onChange={e => {
+            props.editForm(e.target.value==='Not Vegetarian'? false: true, 'vegetarian')
+          }}>
+            <input className="form-check-input" type="radio" value="Not Vegetarian" checked={!props.selectedPizza.vegetarian}/>
             <label className="form-check-label">
               Not Vegetarian
             </label>
           </div>
         </div>
         <div className="col">
-          <button type="submit" className="btn btn-success" onClick={console.log}>Submit</button>
+          <button type="submit" className="btn btn-success" onClick={() => props.updatePizza(props.selectedPizza)}>Submit</button>
         </div>
       </div>
 
